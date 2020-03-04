b35dedd74f9a4e596a39d75006b45cf3e8b2d36c
chrisbchrist <chrisbchrist@gmail.com>
2020-03-04 14:37:08 -0500

    Auto-populating links

--- src/page_action/popup.js
+++ src/page_action/popup.js
@@ -4,23 +4,38 @@ $(document).ready(function() {
       chrome.tabs.sendMessage(tabs[0].id, { action: "READ_LINKS" }, function(
         response
       ) {
+        let lastError = chrome.runtime.lastError;
+        if (lastError) {
+          console.log(lastError);
+          $("#links").append("<h2 class='no-links'>No Links Found</h2>");
+          return;
+        }
         if (response.links) {
+          $("#links").html("");
           $("#links").append(`<div class="link-stats">
                                 <p class="link-count">Total: ${response.links.length}</p>
                             </div>`);
           response.links.forEach(link => {
             $("#links").append(
-              `<div class="link-wrapper"><a href=${link.url} target="_blank">${link.title}</a></div>`
+              `<div class="link-wrapper"><a class="rise-link" href=${link.url} target="_blank">${link.title}</a></div>`
             );
           });
+        } else {
+          $("#links").append("<h2 class='no-links'>No Links Found</h2>")
         }
       });
     });
-
-    console.log("avra' inviato?");
   }
 
-  $("#sendclicks").click(function() {
-    sendClicks();
-  });
+  function copyLinks() {
+
+  }
+
+  // $("#sendclicks").click(function() {
+  //   sendClicks();
+  // });
+  $("#copy-button").click(function() {
+    chrome.tabs.create({url: "src/page_action/page_action.html"});
+  })
+  sendClicks();
 });
